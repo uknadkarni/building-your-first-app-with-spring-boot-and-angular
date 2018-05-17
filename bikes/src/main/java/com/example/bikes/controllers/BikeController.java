e5a8c67b0fed6e64d65efc8bda6c259bd1c344a2
Utkarsh Nadkarni <utkarsh.nadkarni@gmail.com>
2018-05-17 14:40:51 -0400

    Added Delete (one), Get (one)

--- bikes/src/main/java/com/example/bikes/controllers/BikeController.java
+++ bikes/src/main/java/com/example/bikes/controllers/BikeController.java
@@ -4,9 +4,11 @@ import com.example.bikes.models.Bike;
 import com.example.bikes.repositories.BikeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,14 +23,50 @@ public class BikeController {
     }
 
     @GetMapping("/bikes")
+    @ResponseStatus(HttpStatus.FOUND)
     public Collection<Bike> getAll() {
         return bikeRepository.findAll();
     }
 
+
+    @GetMapping("/bikes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Bike get(@PathVariable Long id) {
+        logger.info("Getting: " + id);
+        Optional<Bike> b = bikeRepository.findById(id);
+        if (! ((Optional) b).isPresent()) {
+            logger.info("Bike with id " + id + " doesn't exist");
+        }
+        return b.get();
+    }
+
+
+    @DeleteMapping("/bikes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable Long id) {
+        logger.info("Getting: " + id);
+        bikeRepository.deleteById(id);
+    }
+
+
     @PostMapping("/bikes")
+    @ResponseStatus(HttpStatus.OK)
     public Bike add(@RequestBody Bike bike){
         logger.info("Adding: " + bike);
         return bikeRepository.save(bike);
+    }
+
+    @PutMapping("/bikes/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Bike update(@PathVariable Long id, @RequestBody Bike bike) {
+        logger.info("Updating: " + bike);
+        Optional<Bike> b = bikeRepository.findById(id);
+        if (! ((Optional) b).isPresent()) {
+            logger.info("Bike with id " + id + " doesn't exist");
+        }
+        bike.setId(b.get().getId());
+        Bike savedBike = bikeRepository.save(bike);
+        return savedBike;
     }
 
 }
--- bikes/src/main/java/com/example/bikes/models/Bike.java
+++ bikes/src/main/java/com/example/bikes/models/Bike.java
@@ -31,5 +31,69 @@ public class Bike {
 //    @JsonFormat(pattern = "MM-DD-yyyy", shape = JsonFormat.Shape.STRING)
 //    private Date purchaseDate;
     private boolean contact;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public boolean isContact() {
+        return contact;
+    }
+
+    public void setContact(boolean contact) {
+        this.contact = contact;
+    }
 }
 
